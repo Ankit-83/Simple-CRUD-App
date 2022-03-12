00960a3f56838a3959a43b13653786f7fab9cc27
Ankit Prajapati <65863084+Ankit-83@users.noreply.github.com>
2022-03-12 15:26:07 +0530

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const studentRoutes = require('./src/student/routes');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
