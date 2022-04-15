d6ba9231ba887f1ce6a2adfe078beb438b18051c
Ankit Prajapati <65863084+Ankit-83@users.noreply.github.com>
2022-04-15 15:10:36 +0530

    commit

--- db.js
+++ db.js
@@ -14,8 +14,7 @@ const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD
 const proConfig = process.env.DATABASE_URL
 
 const pool = new Pool({
-    connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig
+    proConfig, devConfig
 });
 
 module.exports = pool;
